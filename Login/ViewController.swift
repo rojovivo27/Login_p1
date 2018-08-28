ec101b9eb51ab98bcf0a591cea35b34aabd76ad8
aldo <rojovivo_27@hotmail.com>
2018-08-28 18:11:08 -0500

    Session 1

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtUser: UITextField!
+    @IBOutlet weak var txtPass: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func hideKeyboard(){
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -19,6 +29,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func login(_ sender: UIButton) {
+        if((txtUser.text?.isEmpty)! || (txtPass.text?.isEmpty)!){
+            let alert = UIAlertController(title: "Error", message: "Fill the blanks!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        } else if (txtUser.text! == "aldo" && txtPass.text! == "aldo"){
+            let alert = UIAlertController(title: "Correcto", message: "Credenciales correctas", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Información incorrecta", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+        
+        
+    }
+    
 }
 
